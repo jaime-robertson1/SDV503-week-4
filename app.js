8e1eb71cad7a8112513692af911fb078d8bed560
jaime-robertson1 <jaime-robertson@live.nmit.ac.nz>
2025-03-12 10:52:26 +1300

    Practice

---
+++ app.js
@@ -0,0 +1,94 @@
+// const firstName = 'Jaime'
+// const lastName = 'Robertson'
+// Camel case naming 
+// const classCode = 'SDV503'
+
+//Invalid vars
+//first-name
+//1_num
+//num_#_1
+
+// Declaring different variables of different data types
+
+// let firstName = 'Jaime' 
+// let lastName = 'Robertson'
+// let country = 'New Zealand'
+// let age = 19
+// let isStudent = true
+
+// console.log(firstName, lastName, country, age,
+// isStudent
+// )
+
+// Declaring variables with number values 
+
+// const gravity = 9.81
+// const boilingPoint = 100
+// const PI = 3.14
+// console.log(gravity, boilingPoint, PI)
+
+//Variables can also be decleared in one line separated by comma
+
+// let name = 'Jaime', job = 'Student', live = 'Nelson'
+// console.log(name, job, live)
+
+
+//
+//Exercise -1 : Variables
+
+// 1. 
+var cat
+var dog
+var frog
+var rabbit
+
+//2. 
+var cat = 'Ralph'
+var dog = 'Kenny'
+var frog = 'Croak'
+var rabbit = 'Betty'
+
+// 3.
+// let firstName = 'Jaime',
+//     lastName= 'Robertson',
+//     studentStatus = true,
+//     country= 'New Zealand',
+//     age= '19'
+
+// console.log(firstName, lastName, studentStatus, country, age)
+
+// 4.
+//let firstName = 'Jaime', lastName = 'Robertson' 
+
+// 5.
+
+// TODO
+/**
+ Data types: 
+
+ 1- Primitive data types
+ 2- Non-primitive data types (Object References)
+
+ // SECTION - Primitive data types:
+ Means primitive data types are immutable data types. 
+ 1- Numbers (7 or 7.1)
+ 2- Strings (any data under single or double quotes)
+ 3- Booleans (true or false)
+ 4- Null (empty value or no value)
+ 5- Undefined (a declared variable without a value)
+
+ // SECTION - Object References:
+ Means Object References are mutable.
+ 1- Objects 
+ 2- Functions 
+ 3- Arrays 
+ EX: let nums = [1,2,3]
+     console.log(nums)
+     nums[2] = 10
+     console.log(nums)
+*/
+
+let nums = [1,2,3]
+     console.log(nums)
+     nums[2] = 10
+     console.log(nums)
